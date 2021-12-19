21aa953a81bdec5a936dc0701961da695043a895
kouyt5 <1147893200@qq.com>
2021-12-20 01:38:28 +0800

    自底向上的字符串正则表达式匹配

--- src/main/java/com/chenc/leetcode/strings/regx/Regr.java
+++ src/main/java/com/chenc/leetcode/strings/regx/Regr.java
@@ -39,6 +39,7 @@ public class Regr {
     }
 
     /**
+     * 自顶向下
      * 
      * @param s
      * @param p
@@ -64,7 +65,7 @@ public class Regr {
                 boolean result = false;
                 for (int i = sIndex; i >= 0; i--) {
                     result = dp(s, p, i, pIndex - 2, globalResult);
-                    globalResult[sIndex][pIndex - 2] = result?1:0;
+                    globalResult[sIndex][pIndex - 2] = result ? 1 : 0;
                     if (result) {
                         globalResult[sIndex][pIndex] = 1;
                         return true; // 只要有一个满足即为true
@@ -83,12 +84,11 @@ public class Regr {
                 // *匹配多个
                 boolean result = false;
                 for (int i = sIndex; i > 0; i--) {
-                    if (s.charAt(i - 1) == p.charAt(pIndex - 2)){
+                    if (s.charAt(i - 1) == p.charAt(pIndex - 2)) {
                         boolean tmpResult = dp(s, p, i, pIndex - 1, globalResult);
-                        globalResult[i][pIndex - 1] = tmpResult?1:0;
+                        globalResult[i][pIndex - 1] = tmpResult ? 1 : 0;
                         result = tmpResult | result;
-                    }
-                    else {
+                    } else {
                         globalResult[i][pIndex] = 0;
                         return false; // 只要有一个满足即为true
                     }
@@ -115,13 +115,79 @@ public class Regr {
         return false;
     }
 
+    /**
+     * 自底向上的方法，效率更高
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean dp2(String s, String p) {
+        int[][] globalResult = new int[s.length() + 1][p.length() + 1];
+        // for(int i = 1;p.length()-1)
+        globalResult[0][0] = 1;
+        for (int i = -1; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) != '*') {
+                    if (i>-1&&(p.charAt(j) == s.charAt(i)||p.charAt(j)=='.')) {
+                        globalResult[i + 1][j + 1] = globalResult[i][j];
+                    } else {
+                        globalResult[i + 1][j + 1] = 0;
+                    }
+                } else {
+                    if (i>-1&&(p.charAt(j-1) == s.charAt(i)||p.charAt(j-1) == '.')){
+                        globalResult[i+1][j+1] = globalResult[i][j+1] | globalResult[i+1][j-1];
+                    }
+                    else{
+                        globalResult[i+1][j+1] = globalResult[i+1][j-1];
+                    }
+                }
+            }
+        }
+        return globalResult[s.length()][p.length()]==1?true:false;
+    }
+
+    public boolean isMatchOnline(String ss, String pp) {
+        // 技巧：往原字符头部插入空格，这样得到 char 数组是从 1 开始，而且可以使得 f[0][0] = true，可以将 true 这个结果滚动下去
+        int n = ss.length(), m = pp.length();
+        ss = " " + ss;
+        pp = " " + pp;
+        char[] s = ss.toCharArray();
+        char[] p = pp.toCharArray();
+        // f(i,j) 代表考虑 s 中的 1~i 字符和 p 中的 1~j 字符 是否匹配
+        boolean[][] f = new boolean[n + 1][m + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // 如果下一个字符是 '*'，则代表当前字符不能被单独使用，跳过
+                if (j + 1 <= m && p[j + 1] == '*')
+                    continue;
+
+                // 对应了 p[j] 为普通字符和 '.' 的两种情况
+                if (i - 1 >= 0 && p[j] != '*') {
+                    f[i][j] = f[i - 1][j - 1] && (s[i] == p[j] || p[j] == '.');
+                }
+
+                // 对应了 p[j] 为 '*' 的情况
+                else if (p[j] == '*') {
+                    f[i][j] = (j - 2 >= 0 && f[i][j - 2])
+                            || (i - 1 >= 0 && f[i - 1][j] && (s[i] == p[j - 1] || p[j - 1] == '.'));
+                }
+            }
+        }
+        return f[n][m];
+    }
+
     public static void main(String[] args) {
         Regr regr = new Regr();
         // String s = "aba";
         // String p = ".*.*";
         // System.out.println((regr.isMatch(s, p)));
-        String s = "a";
-        String p = "c*c*.*";
+        String s = "bac";
+        String p = "b.*";
         System.out.println((regr.isMatch(s, p)));
+        System.out.println(regr.isMatchOnline(s, p));
+        s = "aa";
+        p = "a*";
+        System.out.println(regr.dp2(s, p));
     }
 }
